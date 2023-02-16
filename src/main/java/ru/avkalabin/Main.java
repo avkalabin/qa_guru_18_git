b7ed93cb4c9a0364c68926edc0eb913b31813e50
Andrei Kalabin <a.kalabin@inbox.ru>
2023-02-16 22:42:57 +0500

    added method getMessage

--- src/main/java/ru/avkalabin/Main.java
+++ src/main/java/ru/avkalabin/Main.java
@@ -2,6 +2,10 @@ package ru.avkalabin;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello qa guru!");
+        System.out.println("Hello qa guru!!!!!!!!!!!!!!!!");
+    }
+
+    private static String getMessage() {
+        return "Hello!!!";
     }
 }
