e547d019fe68c5aacf78e088f5c3a786d7734f4d
Andrei Kalabin <a.kalabin@inbox.ru>
2023-02-17 18:44:08 +0500

    2nd println was added

--- src/main/java/ru/avkalabin/Main.java
+++ src/main/java/ru/avkalabin/Main.java
@@ -6,6 +6,7 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println(getMessage());
+        System.out.println(getMessage());
     }
 
     private static String getMessage() {
